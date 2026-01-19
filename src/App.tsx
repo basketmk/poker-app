bd3c1384101f01067445c0ccbd403b950cf8e42c
松原啓斗 <matsubara@crebo.co.jp>
2026-01-19 12:18:18 +0900

    Summaryコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { useForm } from "react-hook-form";
 import { RecordList } from "./components/RecordList";
 import type { RecordItems } from "./types/type";
+import { Summary } from "./components/Summary";
 
 function App() {
   {
@@ -24,42 +25,6 @@ function App() {
     if (!stored) return [];
     return JSON.parse(stored) as RecordItems[];
   });
-
-  {
-    /*==============トータル収支============== */
-  }
-  const totalProfit = records.reduce((sum, r) => sum + (r.buyOut - r.buyIn), 0);
-  {
-    /*=============トータル賞金=============== */
-  }
-  const totalPrize = records.reduce((sum, r) => sum + r.buyOut, 0);
-  {
-    /*=============最大賞金=========== */
-  }
-  const maxPrize = records.reduce((max, r) => Math.max(max, r.buyOut), 0);
-  {
-    /*=============平均buy-in=========== */
-  }
-  const totalBuyIn = records.reduce((sum, r) => sum + r.buyIn, 0);
-  const averageBuyIn =
-    records.length === 0 ? 0 : Math.round(totalBuyIn / records.length);
-  {
-    /*=============トーナメント参加数=========== */
-  }
-  const totalTmCount = records.length;
-  {
-    /*=============インマネ率=========== */
-  }
-  const itmCount = records.filter((r) => r.buyOut > 0).length;
-  const itmRate =
-    records.length === 0 ? "0.0" : ((itmCount / totalTmCount) * 100).toFixed(1);
-  {
-    /*=============ROI=========== */
-  }
-  const Roi =
-    totalBuyIn === 0
-      ? "0.0"
-      : (((totalPrize - totalBuyIn) / totalBuyIn) * 100).toFixed(1);
 
   {
     /* ==============入力フォーム==============*/
@@ -100,52 +65,7 @@ function App() {
       <h1 className="flex items-center justify-center p-4 text-2xl border-b">
         ポーカー収支管理アプリ
       </h1>
-      <div className="grid grid-cols-2">
-        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
-          <p>トータル収支</p>
-          <p
-            className={`${
-              totalProfit > 0
-                ? "text-green-500"
-                : totalProfit < 0
-                  ? "text-red-500"
-                  : "text-black"
-            }`}
-          >
-            {totalProfit >= 0 ? "+" : ""}
-            {totalProfit} USD
-          </p>
-        </div>
-        <div className="grid grid-cols-2">
-          <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
-            <p>インマネ率</p>
-            <div className="flex gap-2">
-              <p className="text-cyan-700">{itmRate}%</p>
-              <p className="text-sm flex items-center justify-center">
-                ({itmCount} / {totalTmCount})
-              </p>
-            </div>
-          </div>
-          <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
-            <p>ROI</p>
-            <p>{Roi}%</p>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-3">
-        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
-          <p>トータル賞金</p>
-          <p className="text-yellow-600">${totalPrize}</p>
-        </div>
-        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
-          <p>最大賞金</p>
-          <p className="text-yellow-600">${maxPrize}</p>
-        </div>
-        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
-          <p>平均buy-in</p>
-          <p>${averageBuyIn}</p>
-        </div>
-      </div>
+      <Summary records={records} />
       {/* ==============新規登録フォーム============== */}
       <div>
         <div className="border-b p-4">＋新規登録</div>
---
+++ src/components/Summary.tsx
@@ -0,0 +1,94 @@
+import type { RecordItems } from "../types/type";
+
+type Props = {
+  records: RecordItems[];
+};
+
+export const Summary = ({ records }: Props) => {
+  {
+    /*==============トータル収支============== */
+  }
+  const totalProfit = records.reduce((sum, r) => sum + (r.buyOut - r.buyIn), 0);
+  {
+    /*=============トータル賞金=============== */
+  }
+  const totalPrize = records.reduce((sum, r) => sum + r.buyOut, 0);
+  {
+    /*=============最大賞金=========== */
+  }
+  const maxPrize = records.reduce((max, r) => Math.max(max, r.buyOut), 0);
+  {
+    /*=============平均buy-in=========== */
+  }
+  const totalBuyIn = records.reduce((sum, r) => sum + r.buyIn, 0);
+  const averageBuyIn =
+    records.length === 0 ? 0 : Math.round(totalBuyIn / records.length);
+  {
+    /*=============トーナメント参加数=========== */
+  }
+  const totalTmCount = records.length;
+  {
+    /*=============インマネ率=========== */
+  }
+  const itmCount = records.filter((r) => r.buyOut > 0).length;
+  const itmRate =
+    records.length === 0 ? "0.0" : ((itmCount / totalTmCount) * 100).toFixed(1);
+  {
+    /*=============ROI=========== */
+  }
+  const Roi =
+    totalBuyIn === 0
+      ? "0.0"
+      : (((totalPrize - totalBuyIn) / totalBuyIn) * 100).toFixed(1);
+
+  return (
+    <div>
+      <div className="grid grid-cols-2">
+        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
+          <p>トータル収支</p>
+          <p
+            className={`${
+              totalProfit > 0
+                ? "text-green-500"
+                : totalProfit < 0
+                  ? "text-red-500"
+                  : "text-black"
+            }`}
+          >
+            {totalProfit >= 0 ? "+" : ""}
+            {totalProfit} USD
+          </p>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
+            <p>インマネ率</p>
+            <div className="flex gap-2">
+              <p className="text-cyan-700">{itmRate}%</p>
+              <p className="text-sm flex items-center justify-center">
+                ({itmCount} / {totalTmCount})
+              </p>
+            </div>
+          </div>
+          <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
+            <p>ROI</p>
+            <p>{Roi}%</p>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-3">
+        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
+          <p>トータル賞金</p>
+          <p className="text-yellow-600">${totalPrize}</p>
+        </div>
+        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
+          <p>最大賞金</p>
+          <p className="text-yellow-600">${maxPrize}</p>
+        </div>
+        <div className="ring ring-zinc-900 rounded-xl mt-3 mr-3 p-4 text-left">
+          <p>平均buy-in</p>
+          <p>${averageBuyIn}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
