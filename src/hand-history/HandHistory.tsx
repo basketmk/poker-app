1f66804c204b4c985aa34aeabba3f1f69ad6b396
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-28 11:25:55 +0900

    HandHistoryからHandList分割

--- src/hand-history/HandHistory.tsx
+++ src/hand-history/HandHistory.tsx
@@ -2,6 +2,7 @@ import type { RecordItems, HandFormValue, HandItem } from "../types/type";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { TournamentList } from "./TournamentList";
+import { HandList } from "./HandList";
 
 type Props = {
   tournaments: RecordItems[];
@@ -178,69 +179,12 @@ export const HandHistory = ({
       </div>
       {/* ==============ハンド履歴============== */}
       {selectedTournament != null && (
-        <div className="flex-1 overflow-y-auto overscroll-contain p-3 w-full">
-          {filteredHands.map((hand) => {
-            const isOpenMemo = selectedMemoId === hand.id;
-
-            return (
-              <div className="border p-2 m-3">
-                <div className="flex items-center justify-between">
-                  <div className="flex p-2 m-2 items-center">
-                    <div className="gap-4 flex">
-                      <div>
-                        <div>(Heroポジション)</div>
-                        <div>{hand.heroPos}</div>
-                      </div>
-                      <div>
-                        <div>(Heroハンド)</div>
-                        <div>{hand.heroHand}</div>
-                      </div>
-                      <div className="text-red-500 flex items-center justify-center">
-                        V S
-                      </div>
-                      <div>
-                        <div>(Vilianポジション)</div>
-                        <div>{hand.vilianPos}</div>
-                      </div>
-                      <div>
-                        <div>(vilianハンド)</div>
-                        <div>{hand.vilianHand}</div>
-                      </div>
-                    </div>
-                  </div>{" "}
-                  <button
-                    className="pr-3 cursor-pointer"
-                    onClick={() => onDeleteHand(hand)}
-                  >
-                    ×
-                  </button>
-                </div>
-                {hand.memo && (
-                  <div className="">
-                    <div className="flex items-left">
-                      <div className="ml-3">メモ</div>
-                      <button
-                        className="ml-3 text-sm rounded-3xl cursor-pointer w-8 h-5"
-                        onClick={() => {
-                          toggleMemo(hand.id);
-                        }}
-                      >
-                        {isOpenMemo ? "▲" : "▼"}
-                      </button>
-                    </div>
-                    {selectedMemoId === hand.id && (
-                      <div className="items-left">
-                        <div className="p-3 m-2 text-sm items-left border rounded-3xl">
-                          {hand.memo}
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                )}
-              </div>
-            );
-          })}
-        </div>
+        <HandList
+          hands={filteredHands}
+          selectedMemoId={selectedMemoId}
+          onDeleteHand={onDeleteHand}
+          toggleMemo={toggleMemo}
+        />
       )}
     </div>
   );
